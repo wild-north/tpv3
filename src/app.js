f1260e68ea864dcfba2a85a617a3d8c55189ee94
Serhii Strizhak <serhii_strizhak@epam.com>
2019-04-26 16:03:57 +0300

    add app file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { AppRouter } from './routes';
+import logo from './logo.svg';
+import './App.css';
 
-export const App = () => {
+function App() {
   return (
-    <div>
-        entry point
-        <AppRouter/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
+export default App;
